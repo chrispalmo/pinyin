7e9eb0a47a7e46a22a61870baea994fa1d374224
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2023-06-10 00:47:06 +0800

    print original chinese characters directly above pinyin

--- pinyin.py
+++ pinyin.py
@@ -8,7 +8,12 @@ def chinese_to_pinyin(text):
     return ' '.join(result)
 
 if len(sys.argv) > 1:
-    print(chinese_to_pinyin(sys.argv[1]))
+    text = sys.argv[1]
+    lines = text.split('\n\n')  # split the text into lines by "double newlines"
+    for line in lines:
+        pinyin = chinese_to_pinyin(line)
+        print(line)  # print the original line
+        print(pinyin)  # print the converted pinyin
+        print()  # print a newline
 else:
     print("Please provide Chinese text as an argument.")
-
