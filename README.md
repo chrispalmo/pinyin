c889b73e72a2b0e2c68e17fab4bbeda3c8d699ff
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2023-06-10 11:12:44 +0800

    output to html file

--- README.md
+++ README.md
@@ -24,6 +24,26 @@ shi jie
 
 The `$'...'` syntax is used in the command to allow the `\n\n` newline characters to be interpreted correctly by the shell.
 
+### HTML Output
+
+To generate an `index.html` file with the pinyin conversion result, add `--html` flag either before or after the Chinese text in your command.
+
+For example:
+
+```bash
+python3 pinyin.py --html $'你好\n世界'
+```
+
+or
+
+```bash
+python3 pinyin.py $'你好\n世界' --html
+```
+
+In both cases, an `index.html` file will be created in the same directory as your script. The output will be formatted such that each line of Chinese text will be immediately followed by its pinyin equivalent, preserving the structure of the input.
+
+Note: If the `index.html` file already exists, running the command with `--html` flag will overwrite the existing file with the new content.
+
 ## Dependencies
 
 The script uses the `jieba` and `pypinyin` libraries for processing the Chinese characters. Make sure you install these Python packages before running the script.
--- pinyin.py
+++ pinyin.py
@@ -5,15 +5,41 @@ import jieba
 def chinese_to_pinyin(text):
     seg_list = jieba.cut(text, cut_all=False)
     result = lazy_pinyin("".join(seg_list), style=Style.TONE3)
-    return ' '.join(result)
+    return ' '.join(result).lstrip()
+
+def create_html_file(output):
+    template = '''<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Hello World</title>
+    <meta name="theme-color" content="#fafafa">
+    <style>
+    </style>
+    </head>
+    <body>
+    <div style="white-space: pre-wrap">
+    {}
+    </div>
+    </body>
+    </html>'''
+    with open('index.html', 'w', encoding='utf-8') as f:
+        f.write(template.format(output))
 
 if len(sys.argv) > 1:
-    text = sys.argv[1]
-    lines = text.split('\n\n')  # split the text into lines by "double newlines"
+    # Check if --html is in the arguments
+    html_flag = '--html' in sys.argv
+    # Remove the --html flag from arguments to leave only the text
+    text = ' '.join(arg for arg in sys.argv[1:] if arg != '--html')
+    
+    lines = text.split('\n')  # split the text into lines by "newlines"
+    output = ''
     for line in lines:
         pinyin = chinese_to_pinyin(line)
-        print(line)  # print the original line
-        print(pinyin)  # print the converted pinyin
-        print()  # print a newline
+        output += line + '\n' + pinyin + '\n'  # construct the output
+    
+    if html_flag:
+        create_html_file(output)
+    else:
+        print(output)
 else:
     print("Please provide Chinese text as an argument.")
